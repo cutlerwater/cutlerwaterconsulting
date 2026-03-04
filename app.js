72e903fb8be27a220b10ca581b615fbb5442b6c3
cutlerwater <cutlerwater2@live.com>
2026-03-03 21:25:12 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,198 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const shuffleBtn = document.getElementById("shuffle");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+const currTime = document.querySelector("#currTime");
+const durTime = document.querySelector("#durTime");
+
+// Song titles
+const songs = ["Beatles- Get Back", "Beethoven 9th Symphony", "Bryan Adams- One Night Love Affair", "Asia- Only Time Will Tell", "Fun- We are young", "Eagles- Doolin Dalton",  "Jurassic Park Theme", "Mozart- Eine Kleine NachtMusik", "Mozart- Marriage of Figaro", "Four Seasons- Cant Take My Eyes Off of You", "Rocky Theme- Gonna Fly Now", "Counting Crows- Hanging Around", "Counting Crows- Einstein On the Beach", "Go Gos- Head Over Heels", "Bryan Adams- Long Gone", "Bruce Springsteen- Im going down", "Barenaked Ladies- One Week", "Blow Monkeys- You dont own me", "Bob Marley- Three Little Birds", 
+"Heart- Nothing at all", "Jefferson Starship- Find Your Way Back", "George Thorogood-I Drink Alone", "Beatles- I Saw Her Standing There", "Bonnie Raitt- Thing Called Love", "Cyndi Lauper- The Goonies R Good Enough",
+"Carl Douglas- Kung Fu Fighting", "Bananarama- Cruel Summer"];
+
+// Keep track of song
+let songIndex = 0;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.m4a`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fa").classList.remove("fa-play");
+  playBtn.querySelector("i.fa").classList.add("fa-pause");
+  playBtn.querySelector("i.fa").classList.add("fa-shuffle");
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fa").classList.add("fa-play");
+  playBtn.querySelector("i.fa").classList.remove("fa-pause");
+  playBtn.querySelector("i.fa").classList.remove("fa-shuffle");
+
+  audio.pause();
+}
+
+function shuffleSong() {
+  playBtn.querySelector("i.fa").classList.add("fa-play");
+  playBtn.querySelector("i.fa").classList.remove("fa-pause");
+  playBtn.querySelector("i.fa").classList.remove("solid fa-shuffle");
+
+  audio.shuffle();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime(e) {
+  const { duration, currentTime } = e.srcElement;
+  var sec;
+  var sec_d;
+
+  // define minutes currentTime
+  let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+  min = min < 10 ? "0" + min : min;
+
+  // define seconds currentTime
+  function get_sec(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec = Math.floor(x) - 60 * i;
+          sec = sec < 10 ? "0" + sec : sec;
+        }
+      }
+    } else {
+      sec = Math.floor(x);
+      sec = sec < 10 ? "0" + sec : sec;
+    }
+  }
+
+  get_sec(currentTime, sec);
+
+  // change currentTime DOM
+  currTime.innerHTML = min + ":" + sec;
+
+  // define minutes duration
+  let min_d = isNaN(duration) === true ? "0" : Math.floor(duration / 60);
+  min_d = min_d < 10 ? "0" + min_d : min_d;
+
+  function get_sec_d(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec_d = Math.floor(x) - 60 * i;
+          sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+        }
+      }
+    } else {
+      sec_d = isNaN(duration) === true ? "0" : Math.floor(x);
+      sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+    }
+  }
+
+  // define seconds duration
+
+  get_sec_d(duration);
+
+  // change duration DOM
+  durTime.innerHTML = min_d + ":" + sec_d;
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// Time/song update
+audio.addEventListener("timeupdate", updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// Song ends
+audio.addEventListener("ended", nextSong);
+
+// Time of song
+audio.addEventListener("timeupdate", DurTime);
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
